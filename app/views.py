da2112ca62d4f8b723b997f120bbe974c7277a6e
j <j@jrm.(none)>
2012-11-25 19:20:05 -0500

    Finished off the polling applicaton.

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 # Create your views here.
 
-from django.template import Context,loader
-from django.shortcuts import render_to_response
+from django.template import Context,loader, RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+from django.core.urlresolvers import reverse
 from django.http import Http404
-from app.models import Poll
-from django.http import HttpResponse
+from app.models import Poll, Choice
+from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
 
@@ -24,10 +25,23 @@ def detail(request,poll_id):
         p = Poll.objects.get(pk=poll_id)
     except Poll.DoesNotExist:
         raise Http404
-    return render_to_response('frontend/detail.html',{'poll':p})
+    return render_to_response('frontend/detail.html',{'poll':p},
+                              context_instance=RequestContext(request))
 
 def results(request,poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('frontend/results.html',{'poll':p})
 
 def vote(request,poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render_to_response('frontend/detail.html',{
+            'poll':p,
+            'error_message':'You didn\'t select a choice.',
+        },context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('app.views.results',args=(p.id,)))
